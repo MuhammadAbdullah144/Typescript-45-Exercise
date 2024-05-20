089ac3223d9b17a0f04bf4fe6135b1b35ccafdde
MuhammadAbdullah144 <166328647+MuhammadAbdullah144@users.noreply.github.com>
2024-05-20 15:59:38 +0500

    Add files via upload

---
+++ 13.Own Array_1.ts
@@ -0,0 +1,7 @@
+//===================Exercise 13===================//
+
+let favorite :string[] = ["honda 125", "Alto 800cc", "Civic honda", "Sportage", "Hilux 4x4", "f16 thunder"]
+//let MyDream :string = "I would like like to owm a "
+for(let i=0; i<favorite.length; i++){
+    console.log("I would like like to own a "+favorite[i])
+}
---
+++ 13.Own Array_2.ts
@@ -0,0 +1,4 @@
+let respected_guest:string[]=["Abid,", "Basharat,", "Tahir,", "Zahid,",]
+for(let i=0; i<respected_guest.length; i++){
+    console.log("Respected Sir"+" "+respected_guest[i]+ "\n invited you on dinner tomorrow \n") 
+}
