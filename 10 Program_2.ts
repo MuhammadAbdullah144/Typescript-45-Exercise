d147ba22b83af69d3bd53084d5106c2b50fcde44
MuhammadAbdullah144 <166328647+MuhammadAbdullah144@users.noreply.github.com>
2024-05-20 15:56:18 +0500

    Add files via upload

---
+++ 10 Program_2.ts
@@ -0,0 +1,4 @@
+// My name is Muhammad Abdullah 
+// Dated: 12-03-24
+// this program will Addition.
+console.log( 5 + 2 ); 
---
+++ 12 Greetings.ts
@@ -0,0 +1,5 @@
+let personsName :string []= ["Abdullah", "Junaid", "Memood", "Tahir", "Shahid"];
+let informed :string = "Tomorrow observed as holiday Sir";
+for(let i=0; i<personsName.length; i++){
+    console.log(informed+" "+personsName[i])
+}
