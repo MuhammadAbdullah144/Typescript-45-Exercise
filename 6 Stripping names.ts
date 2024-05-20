ef91364804345b61bf759fdf64f813d948198b53
MuhammadAbdullah144 <166328647+MuhammadAbdullah144@users.noreply.github.com>
2024-05-20 15:41:25 +0500

    Add files via upload

---
+++ 6 Stripping names.ts
@@ -0,0 +1,4 @@
+let personName: string= "\n\t MUHAMMAD ADULLAH \t\n";
+console.log(personName);
+let stripped: string=  personName.trim();
+console.log(stripped);
---
+++ 7,8 Number eight.ts
@@ -0,0 +1,4 @@
+console.log(3 +5);
+console.log(11 -3);
+console.log(4 *2);
+console.log(16 / 2);
