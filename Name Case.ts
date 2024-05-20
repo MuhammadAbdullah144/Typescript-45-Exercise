966655cb24418c47fdec9e87af00cee4bc52ebba
MuhammadAbdullah144 <166328647+MuhammadAbdullah144@users.noreply.github.com>
2024-05-20 15:34:21 +0500

    Delete Name Case.ts

--- Name Case.ts
+++
@@ -1,6 +0,0 @@
-let personName: string= "Muhammad Abullah"
-console.log("lowercase:", personName.toLowerCase());
-
-console.log("uppercase:", personName.toUpperCase());
-
-console.log("title case:", personName.replace(/\bw/g,c => c.toUpperCase()));
